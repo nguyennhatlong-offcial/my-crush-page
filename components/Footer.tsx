28ec605286f564fdc4cb6fd8537cea5a29c13657
Nguyễn Nhật Long <phamducluong1512@gmail.com>
2026-01-30 20:47:26 +0700

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-6 bg-slate-50 text-center text-slate-400 text-sm">
+      <p>Code with ❤️ by Phạm Đức Lương. Xin chúc mừng hai bạn.</p>
+    </footer>
+  );
+};
+
+export default Footer;
