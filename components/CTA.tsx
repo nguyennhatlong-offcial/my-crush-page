96a8ff37744a732d0f1395d682f110c4c8fdfdd9
Nguyễn Nhật Long <phamducluong1512@gmail.com>
2026-01-30 23:48:51 +0700

    Update EmailJS template ID in CTA component

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -14,7 +14,7 @@ const CTA = () => {
 
   // 2. Cấu hình EmailJS (Điền mã của em vào)
   const SERVICE_ID = "service_fezndmq"; 
-  const TEMPLATE_ID = "template_8xxsu0h"; 
+  const TEMPLATE_ID = "template_00uu5xv"; 
   const PUBLIC_KEY = "OMgJvS5umcTFQOroG";  
 
   const handleAgree = () => {
